7f076b875c8e4fda3accc4fd1caef9ee603d904d
Wingman4l7 <gouldfish@gmail.com>
2019-11-26 13:21:58 -0800

    hotfix -- fix bug with testnet flag in update/create for binance interchain (#9)
    * added fix, version bump, updated changelog

--- src/dctl-interchain-binance-create.js
+++ src/dctl-interchain-binance-create.js
@@ -7,7 +7,7 @@ program
   })
   .arguments('<name>')
   .option('-p, --private-key [privateKey]', '(optional) Base64 or hex-encoded binance private key (or mnemonic) to use with this interchain. Randomly generated if not provided')
-  .option('-t, --testnet', '(optional) Sets this interchain to use the binance testnet (defaults to testnet)')
+  .option('-m, --mainnet', '(optional) Sets this interchain to use the binance mainnet (defaults to testnet)')
   .option('-n, --node-url [nodeURL]', '(optional) The address of the binance node to use (will use a dragonchain-hosted node if not provided')
   .option('-r, --rpc-port [rpcPort]', '(optional) The port used for the RPC endpoints.  Required if providing node-url.')
   .option('-a, --api-port [apiPort]', '(optional) The port used for the API endpoints.  Required if providing node-url.')
@@ -17,13 +17,13 @@ program
 
 util.wrapper(program, async client => {
   const [name] = program.args;
-  const { privateKey, testnet, nodeURL, rpcPort, apiPort } = program;
+  const { privateKey, mainnet, nodeURL, rpcPort, apiPort } = program;
   if (!name) throw new Error("Parameter 'name' must be provided");
   const response = await client.createBinanceInterchain(
     util.removeUndefined({
       name,
       privateKey,
-      testnet,
+      testnet: !mainnet,
       nodeURL,
       rpcPort: rpcPort && Number(rpcPort),
       apiPort: apiPort && Number(apiPort)
--- src/dctl-interchain-binance-update.js
+++ src/dctl-interchain-binance-update.js
@@ -7,7 +7,8 @@ program
   })
   .arguments('<name>')
   .option('-p, --private-key [privateKey]', '(optional) Base64 or hex-encoded binance private key (or mnemonic) to use with this interchain. Randomly generated if not provided')
-  .option('-t, --testnet', '(optional) Sets this interchain to use the binance testnet (defaults to testnet)')
+  .option('-t, --testnet', '(optional) Sets this interchain to use the binance testnet.')
+  .option('-m, --mainnet', '(optional) Sets this interchain to use the binance mainnet.')
   .option('-n, --node-url [nodeURL]', '(optional) The address of the binance node to use (will use a dragonchain-hosted node if not provided')
   .option('-r, --rpc-port [rpcPort]', '(optional) The port used for the RPC endpoints.  Required if providing node-url.')
   .option('-a, --api-port [apiPort]', '(optional) The port used for the API endpoints.  Required if providing node-url.')
@@ -17,13 +18,17 @@ program
 
 util.wrapper(program, async client => {
   const [name] = program.args;
-  const { privateKey, testnet, nodeURL, rpcPort, apiPort } = program;
+  const { privateKey, testnet, mainnet, nodeURL, rpcPort, apiPort } = program;
   if (!name) throw new Error("Parameter 'name' must be provided");
+  let derivedTestnet = mainnet ? false : undefined;
+  if (derivedTestnet === undefined) {
+    derivedTestnet = testnet ? true : undefined;
+  }
   const response = await client.updateBinanceInterchain(
     util.removeUndefined({
       name,
       privateKey,
-      testnet,
+      testnet: derivedTestnet,
       nodeURL,
       rpcPort: rpcPort && Number(rpcPort),
       apiPort: apiPort && Number(apiPort)
