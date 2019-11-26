45750c5a7719613587bd320e13300740e888bf4f
David Gould <wingman4l7@Davids-MacBook-Pro.local>
2019-11-26 13:09:46 -0800

    fixed string

--- src/dctl-interchain-binance-create.js
+++ src/dctl-interchain-binance-create.js
@@ -7,7 +7,7 @@ program
   })
   .arguments('<name>')
   .option('-p, --private-key [privateKey]', '(optional) Base64 or hex-encoded binance private key (or mnemonic) to use with this interchain. Randomly generated if not provided')
-  .option('-m, --mainnet', '(optional)', 'Sets this interchain to use the binance mainnet (defaults to testnet)')
+  .option('-m, --mainnet', '(optional) Sets this interchain to use the binance mainnet (defaults to testnet)')
   .option('-n, --node-url [nodeURL]', '(optional) The address of the binance node to use (will use a dragonchain-hosted node if not provided')
   .option('-r, --rpc-port [rpcPort]', '(optional) The port used for the RPC endpoints.  Required if providing node-url.')
   .option('-a, --api-port [apiPort]', '(optional) The port used for the API endpoints.  Required if providing node-url.')
