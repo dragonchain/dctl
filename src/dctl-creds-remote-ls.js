f86720dd1156af96ed9045cb8e53511103dfc10c
Adam Crowder <adam@adamcrowder.net>
2019-12-04 16:38:23 -0800

    fix formatting
    Signed-off-by: Adam Crowder <adam@adamcrowder.net>

--- src/dctl-creds-remote-ls.js
+++ src/dctl-creds-remote-ls.js
@@ -1,9 +1,7 @@
 const program = require('commander');
 const util = require('./util');
 
-program
-  .description('Get a dragonchain HMAC Credential information by ID')
-  .parse(process.argv);
+program.description('Get a dragonchain HMAC Credential information by ID').parse(process.argv);
 
 util.wrapper(program, async client => {
   const response = await client.listApiKeys();
